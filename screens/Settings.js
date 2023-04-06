40a1eceed356970bf93744d2fa6b37f835354f74
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-04-06 13:05:22 +0300

    ChatApp-with-React-Native-Paper-Firebase-and-Navigation6

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Avatar, Title, Subheading, Button } from "react-native-paper";
 import { View, Text, StyleSheet } from "react-native";
 
 const Settings = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   return (
-    <View>
-      <Text>Settings</Text>
+    <View style={{ alignItems: "center", marginTop: 16 }}>
+      <Avatar.Text label="YL" size={56} />
+      <Title>User Name"</Title>
+      <Subheading>User Email</Subheading>
+      <Button style={{ marginTop: 6 }}>Sign Out</Button>
     </View>
   );
 };
